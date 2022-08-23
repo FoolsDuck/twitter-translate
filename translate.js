f50aa4a22ae1abac4c4c572fd99c6976450c5d95
topshorter <fullsduck@gmail.com>
2022-08-23 20:56:02 +0300

    ignore tags

--- translate.js
+++ translate.js
@@ -1,8 +1,15 @@
 const translate = require("translate-google");
 
 const translateTweet = (str, lang) => {
-  return translate(str, { to: lang })
-    .then((res) => res)
+  const tagRe = /\@\w+/g;
+  const original_tags = str.match(tagRe);
+  const cleanStr = str.replace(tagRe, "");
+  return translate(cleanStr, { to: lang })
+    .then((res) =>
+      original_tags && original_tags.length > 0
+        ? `${res} ${original_tags.join(" ")}`
+        : res
+    )
     .catch((err) => err);
 };
 
